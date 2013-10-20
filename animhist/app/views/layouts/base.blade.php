9409ad9791bc12343a025941f67ab77099e2956d
Zuyet Awarmatik <francesc.zuyet@gmail.com>
2013-10-20 16:12:59 +0800

    Setup prototype

---
+++ animhist/app/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>Animated History</title>
+		
+		<!-- Stylesheet -->
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic&subset=latin,vietnamese,latin-ext' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet/less" type="text/css" href="css/base.less.css" />
+		{{ HTML::style('css/reset.css'); }}
+		<!---------------->
+		
+		<!-- Script -->
+		{{ HTML::script('js/less-1.4.2.min.js'); }}
+		<!---------------->
+		
+	</head>
+	<body>
+		<section id="left-panel"></section>
+		<section id="main-panel"></section>
+	</body>
+</html>
