c5066edbe7a6924130d2e7f6c33fff1ce8cca95e
Zuyet Awarmatik <francesc.zuyet@gmail.com>
2013-11-21 00:49:33 +0800

    Setting form

--- animhist/app/views/settings.blade.php
+++ animhist/app/views/settings.blade.php
@@ -5,5 +5,21 @@
 @stop
 
 @section('left-area')
-
+	{{ Form::open(array('name'=>'settings-form', 'url'=>'')) }}
+		<table>
+			<tr>
+				<td>{{ Form::label('display-name', 'Display Name:') }}</td>
+				<td>{{ Form::text('display-name', 'Francesc') }}</td>
+			</tr>
+			<tr>
+				<td>{{ Form::label('description', 'Brief Description:') }}</td>
+				<td>{{ Form::textarea('description', 'Lorem ipsum dolor sit amet') }}</td>
+			</tr>
+			<tr>
+				<td>{{ Form::label('password', 'Password:') }}</td>
+				<td>{{ Form::button('Change Password...', array('name'=>'changepwd-btn')) }}</td>
+			</tr>
+		</table>
+		<!-- {{ Form::submit('Post', array('name'=>'submit-btn')) }} -->
+	{{ Form::close() }}
 @stop
