e952499f2f3af21da09c037a79a532d52769d42c
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-02-11 16:46:31 +0800

    Better Size for small device screen

--- animhist/public/js/project/main-shared.js
+++ animhist/public/js/project/main-shared.js
@@ -37,17 +37,31 @@ $(function() {
 	/* Right sidebar show-hide */
 	var rightSidebar = $("#right-area");
 	var mainArea = $("#left-area");
+	
 	$("#right-area-showhide-btn").click(function() {
+		var rightAreaWidth = rightSidebar.width();
+		var rightAreaTabWidth = $("#right-area-tab").length ? $("#right-area-tab").width() : 0;
 		if (rightSidebar.hasClass("hidden")) {
 			rightSidebar.stop(true).animate({right: "0"}, 400, "easeOutQuad");
-			mainArea.stop(true).animate({right: "440px"}, 400, "easeOutQuad", mapResizeTrigger);
+			mainArea.stop(true).animate({right: rightAreaWidth + "px"}, 400, "easeOutQuad", mapResizeTrigger);
 			$(this).html("&#57477;");
 		} else {
-			rightSidebar.stop(true).animate({right: "-480px"}, 400, "easeOutQuad");
-			mainArea.stop(true).animate({right: "-40px"}, 400, "easeOutQuad", mapResizeTrigger);
+			rightSidebar.stop(true).animate({right: (-rightAreaWidth - rightAreaTabWidth) + "px"}, 400, "easeOutQuad");
+			mainArea.stop(true).animate({right: -rightAreaTabWidth + "px"}, 400, "easeOutQuad", mapResizeTrigger);
 			$(this).html("&#57528;");
 		}
 		rightSidebar.toggleClass("hidden");
+	});
+	
+	$(window).resize(function() {
+		var rightAreaWidth = rightSidebar.width();
+		var rightAreaTabWidth = $("#right-area-tab").length ? $("#right-area-tab").width() : 0;
+		if (rightSidebar.hasClass("hidden")) {
+			rightSidebar.css({right: (-rightAreaWidth - rightAreaTabWidth) + "px"});
+			mainArea.css({right: -rightAreaTabWidth + "px"});
+		} else {
+			mainArea.css({right: rightAreaWidth + "px"});
+		}
 	});
 	
 	/* Right category area animation */
